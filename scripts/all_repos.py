c9bda8f5afadedc1011f1fecd8034daea329b1df
heulitig <rithik@fifthtry.com>
2023-03-28 17:03:37 +0530

    fastn-community added under artifacts.ftd

--- scripts/all_repos.py
+++ scripts/all_repos.py
@@ -2,7 +2,7 @@ import subprocess
 import json
 from datetime import datetime
 
-orgs = ["fifthtry", "fastn-stack"]
+orgs = ["fifthtry", "fastn-stack", "fastn-community"]
 
 org_vice_repos = {}
 
@@ -59,7 +59,7 @@ for org in orgs:
     org_vice_repos[org] = repos
 
 # Write the list of dictionaries to a JSON file
-with open("artifacts.json", "w") as f:
+with open("../artifacts.json", "w") as f:
     json.dump(org_vice_repos, f)
 
 # Print a confirmation message
